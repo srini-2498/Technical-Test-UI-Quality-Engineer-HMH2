3aa417b9e83742b545116df25c945ad8762a5fb3
srini-2498 <80764818+srini-2498@users.noreply.github.com>
2025-03-05 11:27:25 +0000

    Update TodoTest.java

--- hmh-todomvc-ui-test-simplified/src/test/java/TodoTest.java
+++ hmh-todomvc-ui-test-simplified/src/test/java/TodoTest.java
@@ -1,15 +1,19 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -37,14 +41,23 @@ public class TodoTest {
         WebElement newTodo = driver.findElement(By.className("new-todo"));
         newTodo.sendKeys("Buy Milk" + Keys.ENTER);
 
-        WebElement todoLabel = driver.findElement(By.cssSelector(".todo-list li label"));
-        WebElement todoItem = driver.findElement(By.cssSelector(".todo-list li"));
-((JavascriptExecutor) driver).executeScript("arguments[0].click();", todoLabel);
-//        driver.executeScript("arguments[0].click();", todoLabel);
-        WebElement editInput = todoItem.findElement(By.className("edit"));
+        // Wait for the todo item to be added
+        WebElement todoLabel = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".todo-list li label")));
+
+        // Double-click the label to enter edit mode
+        Actions actions = new Actions(driver);
+        actions.doubleClick(todoLabel).perform();
+
+        // Wait for the edit input field to be visible
+        WebElement editInput = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".todo-list li .edit")));
+
+        // Clear the existing text and enter new text
         editInput.clear();
         editInput.sendKeys("Buy Coffee" + Keys.ENTER);
 
+        // Verify the updated todo item
         List<String> todos = driver.findElements(By.cssSelector(".todo-list li label"))
                                    .stream().map(WebElement::getText).collect(Collectors.toList());
         Assert.assertTrue(todos.contains("Buy Coffee"));
@@ -56,7 +69,8 @@ public class TodoTest {
         newTodo.sendKeys("Buy Milk" + Keys.ENTER);
 
         WebElement destroyButton = driver.findElement(By.cssSelector(".todo-list li .destroy"));
-        driver.executeScript("arguments[0].click();", destroyButton);
+        // Cast driver to JavascriptExecutor and execute the script
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", destroyButton);
 
         List<WebElement> todos = driver.findElements(By.cssSelector(".todo-list li"));
         Assert.assertEquals(todos.size(), 0);
